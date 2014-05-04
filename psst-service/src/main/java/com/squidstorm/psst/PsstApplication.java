c7d97a8a529a51c27a2f9c0227f4a0e39c438a44
Simon Nattrass <simon@squidstorm.com>
2014-05-04 14:57:00 +0100

    Giving PSST a view

--- psst-service/src/main/java/com/squidstorm/psst/PsstApplication.java
+++ psst-service/src/main/java/com/squidstorm/psst/PsstApplication.java
@@ -2,6 +2,9 @@ package com.squidstorm.psst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 /**
@@ -15,5 +18,16 @@ public class PsstApplication
     public static void main (String[] args) throws Exception
     {
         SpringApplication.run(PsstApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurerAdapter mvcViewConfigurer()
+    {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/client").setViewName("client");
+            }
+        };
     }
 }
---
+++ psst-service/src/main/java/com/squidstorm/psst/controllers/PsstController.java
@@ -0,0 +1,4 @@
+package com.squidstorm.psst.controllers;
+
+public class PsstController {
+}
