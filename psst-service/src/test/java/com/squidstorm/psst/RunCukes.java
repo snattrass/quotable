cf0bc467023b1ecaab950656920bc5cadabad815
Simon Nattrass <simon@squidstorm.com>
2014-05-14 19:23:42 +0100

    Cuking it

---
+++ psst-service/src/test/java/com/squidstorm/psst/RunCukes.java
@@ -0,0 +1,12 @@
+package com.squidstorm.psst;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true)
+public class RunCukes
+{
+}
