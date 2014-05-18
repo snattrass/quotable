37d65eb64f5faf7ed264cebdc94b159a05703f70
Simon Nattrass <simon@squidstorm.com>
2014-05-18 09:48:52 +0100

    Better output

--- psst-service/src/test/java/com/squidstorm/psst/RunCukesTest.java
+++ psst-service/src/test/java/com/squidstorm/psst/RunCukesTest.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true)
+@CucumberOptions(format = "pretty")
 public class RunCukesTest
 {
 }
