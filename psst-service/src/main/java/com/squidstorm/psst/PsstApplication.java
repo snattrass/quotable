5e08adce4919c34314740e8eddabab3c904d576c
Simon Nattrass <simon@squidstorm.com>
2014-05-31 11:20:31 +0100

    Set our configuration

--- psst-service/src/main/java/com/squidstorm/psst/PsstApplication.java
+++ psst-service/src/main/java/com/squidstorm/psst/PsstApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  *
  * @author Simon Nattrass
  */
+@Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class PsstApplication
