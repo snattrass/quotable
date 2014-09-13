5d4bc9a332109d792bf0a11719ffaa28cbe52d51
Simon Nattrass <simon@squidstorm.com>
2014-09-13 14:03:56 +0100

    Ignoring Cukes for the time being

--- psst-service/src/test/java/com/squidstorm/psst/RunCukesTest.java
+++ psst-service/src/test/java/com/squidstorm/psst/RunCukesTest.java
@@ -3,10 +3,14 @@ package com.squidstorm.psst;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(format = "pretty")
+
+
+@Ignore
 public class RunCukesTest
 {
 }
