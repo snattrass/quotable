fd3ccdd0f283f1f962d8d4a955aaa0459550a27d
Simon Nattrass <simon@squidstorm.com>
2014-09-14 17:25:14 +0100

    Enable the H2 Console and TcpServer for in memory testing.  To be moved to src/test later.

--- src/main/java/com/squidstorm/psst/PsstApplication.java
+++ src/main/java/com/squidstorm/psst/PsstApplication.java
@@ -1,12 +1,18 @@
 package com.squidstorm.psst;
 
+import org.h2.server.web.WebServlet;
+import org.h2.tools.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.sql.SQLException;
 
 
 /**
@@ -33,5 +39,19 @@ public class PsstApplication
                 registry.addViewController("/client").setViewName("client");
             }
         };
+    }
+
+    @Bean
+    public ServletRegistrationBean h2Console() throws SQLException
+    {
+        ServletRegistrationBean reg = new ServletRegistrationBean(new WebServlet(), "/console/*");
+        reg.setLoadOnStartup(1);
+        return reg;
+    }
+
+    @Bean
+    public Server h2TcpServer() throws SQLException
+    {
+        return Server.createTcpServer().start();
     }
 }
