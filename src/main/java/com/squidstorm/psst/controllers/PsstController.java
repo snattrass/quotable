396a16aab3d7ab572fd3a19c2bf1d9c2accf3e03
Simon Nattrass <simon@squidstorm.com>
2014-09-15 13:02:18 +0100

    Dumping all quotes via REST

--- src/main/java/com/squidstorm/psst/controllers/PsstController.java
+++ src/main/java/com/squidstorm/psst/controllers/PsstController.java
@@ -1,15 +1,25 @@
 package com.squidstorm.psst.controllers;
 
+import com.squidstorm.psst.domain.Quote;
+import com.squidstorm.psst.service.QuoteRepository;
+import org.apache.commons.collections4.IteratorUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/")
 public class PsstController
 {
+    @Autowired
+    private QuoteRepository quoteRepository;
+
     @RequestMapping("quotes")
-    public String secrets() {
-        return "Here is a list of all quotes";
+    public List<Quote> getAll() {
+        Iterable<Quote> quotes = quoteRepository.findAll();
+        return IteratorUtils.toList(quotes.iterator());
     }
 }
