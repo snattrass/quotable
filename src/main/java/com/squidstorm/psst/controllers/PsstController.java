45e5dec49ba27c4cae76b28f024c1597dc744110
Simon Nattrass <simon@squidstorm.com>
2014-09-15 13:07:44 +0100

    Returning a single quote

--- src/main/java/com/squidstorm/psst/controllers/PsstController.java
+++ src/main/java/com/squidstorm/psst/controllers/PsstController.java
@@ -4,6 +4,7 @@ import com.squidstorm.psst.domain.Quote;
 import com.squidstorm.psst.service.QuoteRepository;
 import org.apache.commons.collections4.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +12,20 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/quotes")
 public class PsstController
 {
     @Autowired
     private QuoteRepository quoteRepository;
 
-    @RequestMapping("quotes")
+    @RequestMapping("")
     public List<Quote> getAll() {
         Iterable<Quote> quotes = quoteRepository.findAll();
         return IteratorUtils.toList(quotes.iterator());
+    }
+
+    @RequestMapping("/{id}")
+    public Quote getOne(@PathVariable Long id) {
+        return quoteRepository.findOne(id);
     }
 }
